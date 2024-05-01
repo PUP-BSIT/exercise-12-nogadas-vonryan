786e1f7eec23175d9c31e9524d30ac975ac352f0
nogadas-vonryan <vonnogadas251@gmail.com>
2024-05-01 15:57:33 +0800

    get elements in js with querySelectors

---
+++ script.js
@@ -0,0 +1,6 @@
+let counterElement = document.querySelector("#counter")
+let startButton = document.querySelector("#start_button");
+let pauseButton = document.querySelector("#pauseButton");
+let resumeButton = document.querySelector("#resume_button");
+let stopButton = document.querySelector("#stopButton");
+let counter = 0;
