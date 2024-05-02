1050cdd22096df6f2247ee8d5dd48638032fdf12
nogadas-vonryan <vonnogadas251@gmail.com>
2024-05-02 14:55:15 +0800

    add function to set button states to disabled or enabled

--- script.js
+++ script.js
@@ -12,22 +12,33 @@ function addCounter() {
 	counterElement.textContent = counter;
 }
 
+function setButtonStates(start, pause, resume, stop) {
+	startButton.disabled = start;
+	pauseButton.disabled = pause;
+	resumeButton.disabled = resume;
+	stopButton.disabled = stop;
+}
+
 function startTimer() {
 	counter = 0;
 	counterElement.textContent = counter;
 	intervalId = setInterval(addCounter, 1000);
+	setButtonStates(true, false, true, false);
 }
 
 function pauseTimer() {
 	clearInterval(intervalId);
+	setButtonStates(true, true, false, false);
 }
 
 function resumeTimer() {
 	intervalId = setInterval(addCounter, 1000);
+	setButtonStates(true, false, true, false);
 }
 
 function stopTimer() {
 	counter = 0;
 	counterElement.textContent = counter;
 	clearInterval(intervalId);
+	setButtonStates(false, true, true, true);
 }
